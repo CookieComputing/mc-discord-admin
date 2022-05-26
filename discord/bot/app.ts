deb2fb28fc54ad654a83cdd9f5174db271adc11d
Kevin Hui <khui.cs@gmail.com>
2022-05-25 20:12:44 -0400

    add start of the discord bot

---
+++ discord/bot/app.ts
@@ -0,0 +1,10 @@
+import { DiscordCommand } from "./commands";
+
+/**
+ * Processes the slash command received by the serverless function.
+ * @param command A discord command made by a user
+ * @returns The result of the interaction
+ */
+export function processCommand(command: DiscordCommand) {
+    return "";
+}
---
+++ discord/bot/commands.ts
@@ -0,0 +1,14 @@
+import { InteractionType } from "discord-interactions";
+
+/**
+ * A slash command provided by an admin or otherwise authorized user. This 
+ * is a one way message passed from the serverless function to the bot.
+ */
+export interface DiscordCommand {
+
+    // The type of command that was used.
+    type: InteractionType,
+
+    // data provided by the command
+    data: string | null;
+}
