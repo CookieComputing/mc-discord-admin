2b985a749463e84b7806c0076d4fc2e255f0420b
Kevin Hui <khui.cs@gmail.com>
2022-05-27 22:09:19 -0400

    add fix for official connection from discord to azure function

--- cloud/azure/DiscordAzureFunction/DiscordAzureFunc/index.ts
+++ cloud/azure/DiscordAzureFunction/DiscordAzureFunc/index.ts
@@ -1,15 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { verifySignature } from '../../../../discord/bot/auth/slash_auth';
 import { publicKey } from '../../../../discord/bot/env';
+import { InteractionType, InteractionResponseType } from 'discord-api-types/v10';
 
 const httpTrigger: AzureFunction = async function(context: Context, req: HttpRequest): Promise<void> {
 	context.log('HTTP trigger function processed a request.');
-
-	context.log(req.headers['X-Signature-Ed25519']);
-	if (!verifySignature(req.headers['X-Signature-Ed25519'],
-		req.headers['X-Signature-Timestamp'],
+	context.log(req.headers);
+	if (!verifySignature(req.headers['x-signature-ed25519'],
+		req.headers['x-signature-timestamp'],
 		req.rawBody,
-		publicKey)) {
+		publicKey as string)) {
 		context.res = {
 			// Unauthorized
 			status: 401,
@@ -17,6 +17,19 @@ const httpTrigger: AzureFunction = async function(context: Context, req: HttpReq
 		};
 		return;
 	}
+
+	if (req.body.type === InteractionType.Ping) {
+		console.log('Received ping');
+		context.res = {
+			status: 200,
+			body: {
+			type: InteractionResponseType.Pong,
+
+			},
+		};
+		return;
+	}
+	context.log(req.rawBody);
 
 	context.res = {
 		// status: 200, /* Defaults to 200 */
