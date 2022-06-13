3802511de225c9619af17e03df2f520024b8a218
Kevin Hui <khui.cs@gmail.com>
2022-06-12 22:30:36 -0400

    add template for registration

--- discord/bot/register/register-commands.ts
+++ discord/bot/register/register-commands.ts
@@ -1,4 +1,21 @@
 import { REST } from '@discordjs/rest';
-import { appId, token, guildId, publicKey } from '../env';
+import { Routes } from 'discord-api-types/v10';
+import { appId, token, guildId } from '../env';
 
 const rest = new REST().setToken(token);
+
+(async () => {
+  try {
+    console.log('Starting slash command registration');
+
+    await rest.put(
+      Routes.applicationGuildCommands(appId, guildId),
+      { body: ['TODO'] },
+    );
+
+    console.log('Successfully registered commands');
+  }
+  catch (error) {
+    console.error(error);
+  }
+})();
