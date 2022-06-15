f6ce11cead8eec1c4537d47eb67e73faa2b25c83
Kevin Hui <khui.cs@gmail.com>
2022-06-14 21:22:24 -0400

    remove all files associated with discord.js

--- discord/bot/commands/commands.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
-import { data as pongData, action as pongAction } from './pong';
-
-type DiscordAction = (interaction: CommandInteraction) => Promise<void>;
-type DiscordCommand = [SlashCommandBuilder, DiscordAction];
-
-export const commands: [DiscordCommand] = [[pongData, pongAction]];
