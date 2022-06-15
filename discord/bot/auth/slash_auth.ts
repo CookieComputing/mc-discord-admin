d9e339aa974e87feeed9cc3570188e4542beabf4
Kevin Hui <khui.cs@gmail.com>
2022-06-14 21:31:32 -0400

    change verifySignature() implementation

--- discord/bot/auth/slash_auth.ts
+++ discord/bot/auth/slash_auth.ts
@@ -1,5 +1,4 @@
-import { sign_detached_verify } from 'tweetnacl-ts';
-
+import { verifyKey } from 'discord-interactions';
 // The authentication module for verifying discord slash commands are coming from
 // the application, and not from some other random traffic.
 /**
@@ -10,9 +9,7 @@ import { sign_detached_verify } from 'tweetnacl-ts';
  * @param pubKey The application public key
  */
 export function verifySignature(signature: string, timestamp: string, body: string, pubKey: string): boolean {
-  return sign_detached_verify(
-    Buffer.from(timestamp + body),
-    Buffer.from(signature, 'hex'),
-    Buffer.from(pubKey, 'hex'),
-  );
+  // This is a little redundant, but leaving this here until the discord API
+  // being used for the project is settled.
+  return verifyKey(body, signature, timestamp, pubKey);
 }
