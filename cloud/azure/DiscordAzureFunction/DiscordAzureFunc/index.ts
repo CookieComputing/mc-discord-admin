e9707b2926617ee6be0beb9857a69c4e8bcd944e
Kevin Hui <khui.cs@gmail.com>
2022-06-14 22:19:44 -0400

    add init for azure functions using slash-create

--- cloud/azure/DiscordAzureFunction/DiscordAzureFunc/index.ts
+++ cloud/azure/DiscordAzureFunction/DiscordAzureFunc/index.ts
@@ -1,39 +1,17 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-import { verifySignature } from '../../../../discord/bot/auth/slash_auth';
-import { publicKey } from '../../../../discord/bot/env';
-import { InteractionType, InteractionResponseType } from 'discord-api-types/v10';
+import path = require('path');
+import { AzureFunctionServer, SlashCreator } from 'slash-create';
+import { appId, publicKey, token } from '../../../../discord/bot/env';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log('HTTP trigger function processed a request.');
-  context.log(req.headers);
-  if (!verifySignature(req.headers['x-signature-ed25519'],
-    req.headers['x-signature-timestamp'],
-    req.rawBody,
-    publicKey as string)) {
-    context.res = {
-      // Unauthorized
-      status: 401,
-      body: 'Given invalid signature',
-    };
-    return;
-  }
+const creator = new SlashCreator({
+  applicationID: appId,
+  publicKey: publicKey,
+  token: token,
+});
 
-  if (req.body.type === InteractionType.Ping) {
-    console.log('Received ping');
-    context.res = {
-      status: 200,
-      body: {
-        type: InteractionResponseType.Pong,
-      },
-    };
-    return;
-  }
-  context.log(req.rawBody);
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: 'Hello, world!',
-  };
-};
-
-export default httpTrigger;
+creator
+  // The first argument is required, but the second argument is the "target" or the name of the export.
+  // By default, the target is "interactions".
+  .withServer(new AzureFunctionServer(module.exports))
+  .registerCommandsIn(path.join(__dirname, 'commands'))
+  // Syncing the commands each time the function is executed is wasting computing time
+  .syncCommands();
