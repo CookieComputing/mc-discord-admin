d3e86d20cd971b47fc2cca4165fba5be755e7192
Kevin Hui <khui.cs@gmail.com>
2022-07-04 17:37:39 -0400

    add start of script file for creating config table manager

---
+++ scripts/create-config-manager-table.ts
@@ -0,0 +1,29 @@
+import { command, run, string, positional, Type } from 'cmd-ts';
+import { CloudVendor } from '../discord/bot/vendors';
+
+/**
+ * A script you can run to create a config manager table in a supported
+ * cloud vendor of your choice.
+ */
+
+const cloudPlatform: Type<string, CloudVendor> = {
+  async from(str) {
+    if (Object.values(CloudVendor).includes(str as CloudVendor)) {
+      return str as CloudVendor;
+    }
+
+    throw new Error(`Provided an unsupported cloud vendor: ${str}. Expected: ${Object.values(CloudVendor)}`);
+  },
+};
+
+const app = command({
+  name: 'create-config-manager-table',
+  args: {
+    someArg: positional({ type: cloudPlatform, displayName: 'some arg'}),
+  },
+  handler: ({ someArg }) => {
+    console.log({ someArg });
+  },
+});
+
+run(app, process.argv.slice(2));
